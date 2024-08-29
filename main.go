9381f1b775ef8b9da9a1ac6485bc95399c09cf47
Jerry Kingsbury <jerrykingsbury@Jerrys-MacBook-Pro.local>
2024-08-28 22:01:41 -0500

    Saving progress on creating the tree using bubble tea

--- main.go
+++ main.go
@@ -1,50 +1,104 @@
 package main
 
 import (
-	"github.com/rivo/tview"
+	"fmt"
+
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/lipgloss/tree"
 )
 
-// Need
-// - Connections Pane
-// - DB Side Bar
-// - SQL Input Bar
-// - Table Results pane
-// - Shortcut commands on the bottom of the screen
-// - Help window with list of commands
-
-// Maybe
-// - Saved Queries Pane
-// - Query History Pane
-
-// TODO: Check to see how to style the list and place into a pane window
-func CreateDBSideBar(app *tview.Application) *tview.List {
-	dbSidebar := tview.NewList()
-
-	dbSidebar.AddItem("New query", "", 'n', nil).
-		AddItem("Saved Queries", "", 's', nil).
-		AddItem("Query History", "", 'h', nil).
-		AddItem("Schemas", "", 'c', nil).
-    // For testing
-		AddItem("Quit", "Press to exit", 'q', func() {
-			app.Stop()
-		})
-
-	return dbSidebar
+type styles struct {
+	base,
+	block,
+	enumerator,
+	toggle,
+	db,
+	table,
+	column lipgloss.Style
 }
 
-func CreateLayout(app *tview.Application) *tview.Flex {
-	dbSidebar := CreateDBSideBar(app)
-
-	layout := tview.NewFlex().AddItem(dbSidebar, 20, 1, true)
-
-	return layout
+func defaultStyles() styles {
+	var s styles
+	s.base = lipgloss.NewStyle().
+		Background(lipgloss.Color("57")).
+		Foreground(lipgloss.Color("225"))
+	s.block = s.base.
+		Padding(1, 3).
+		Margin(1, 3).
+		Width(40)
+	s.enumerator = s.base.
+		Foreground(lipgloss.Color("212")).
+		PaddingRight(1)
+	s.toggle = s.base.
+		Foreground(lipgloss.Color("207")).
+		PaddingRight(1)
+	s.db = s.base
+	s.table = s.base
+	s.column = s.base
+	return s
 }
 
-func main() {
-	app := tview.NewApplication()
-	layout := CreateLayout(app)
+type db struct {
+	name   string
+	open   bool
+	styles styles
+}
 
-	if err := app.SetRoot(layout, true).Run(); err != nil {
-		panic(err)
+func (d db) String() string {
+	t := d.styles.toggle.Render
+	n := d.styles.db.Render
+	if d.open {
+		return t("▼") + n(d.name)
 	}
+	return t("▶") + n(d.name)
+}
+
+type table struct {
+	name   string
+	open   bool
+	styles styles
+}
+
+func (t table) String() string {
+	toggle := t.styles.toggle.Render
+	tableName := t.styles.table.Render
+
+	if t.open {
+		return toggle("▼") + tableName(t.name)
+	}
+	return toggle("▶") + tableName(t.name)
+}
+
+type column struct {
+  name string
+  styles styles
+}
+
+func (c column) String() string {
+  return c.styles.column.Render(c.name)
+}
+
+func PostgresTreeView() {
+  s := defaultStyles()
+
+	t := tree.Root(db{"DB Name", true, s}).
+    Enumerator(tree.RoundedEnumerator).
+    EnumeratorStyle(s.enumerator).
+		Child("New Query").
+		Child("Saved Queries").
+		Child(
+			tree.New().
+				Root("Schemas").
+				Child("Information Schema").
+				Child("PG Catalog").
+				Child("PG Toast").
+				Child("Public"),
+		)
+
+	fmt.Println(s.block.Render(t.String()))
+}
+
+// Tree view using lipgloss
+func main() {
+	PostgresTreeView()
 }
